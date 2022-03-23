57924b33b2ad0342759a8466c6ec54cfd9ed3f23
gianlucaparadise <gianlucaparadise@gmail.com>
2022-03-23 08:37:17 +0100

    Simple album item

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,6 @@
+const find = require("lodash/fp/find");
+const first = require("lodash/fp/first");
+
 const inspect = require("util").inspect;
 
 module.exports = function (eleventyConfig) {
@@ -9,4 +12,16 @@ module.exports = function (eleventyConfig) {
     "debug",
     (content) => `<pre>${inspect(content)}</pre>`
   );
+
+  eleventyConfig.addFilter("year", (dateString) => {
+    const date = new Date(dateString);
+    return date.getFullYear();
+  });
+
+  eleventyConfig.addFilter("album-cover-url", (album) => {
+    const isAround300 = (image) => Math.abs(image.width - 300) < 25;
+    const optimalImage = find(isAround300, album.images);
+    const image = optimalImage || first(album.images);
+    return image?.url;
+  });
 };
