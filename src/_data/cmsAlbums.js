06f890febce3e3890bfeaaf39ddbcb458b64a793
gianlucaparadise <gianlucaparadise@gmail.com>
2022-04-05 08:11:40 +0200

    Renamed Overview in Fresh Finds

--- src/_data/cmsAlbums.js
+++ src/_data/cmsAlbums.js
@@ -15,12 +15,25 @@ const fetchAlbumsFromCms = async function () {
     "sort[0]": "releaseYear",
   }).toString();
 
-  const response = await fetch(url, {
+  const opts = {
     headers: {
       Authorization: `bearer ${cmsApiKey}`,
     },
-  });
+  };
+
+  console.info("fetchAlbumsFromCms - Fetching from:", url, "with opts:", opts);
+
+  const response = await fetch(url, opts);
+
   const data = await response.json();
+
+  if (data.error) {
+    console.error("Error while fetching cmsAlbums:", data.error);
+    throw new Error("Error while fetching cmsAlbums");
+  }
+
+  console.info("fetchAlbumsFromCms - Fetched:", data);
+
   return data;
 };
 
@@ -41,6 +54,11 @@ const toAlbum = (item) => ({
 module.exports = async function () {
   try {
     const cmsAlbums = await fetchAlbumsFromCms();
+    if (!cmsAlbums?.data) {
+      console.info("cmsAlbums - Empty album list", cmsAlbums);
+      return [];
+    }
+
     return cmsAlbums.data.map(toAlbum);
   } catch (e) {
     console.error("Error while building cmsAlbums", e);
